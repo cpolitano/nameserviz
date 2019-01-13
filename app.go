c4f5bda0c856c411779db7be32c6df44c04dd249
Clare Politano Hutchings <cpolitano8@gmail.com>
2019-01-13 15:21:43 -0600

    embed baseapp

--- app.go
+++ app.go
@@ -1,9 +1,31 @@
 package app
 
 import (
-  "github.com/tendermint/tendermint/libs/log" // Tendermint logger
-	"github.com/cosmos/cosmos-sdk/x/auth" // auth module
+	"github.com/cosmos/cosmos-sdk/x/auth"       // auth module
+	"github.com/tendermint/tendermint/libs/log" // Tendermint logger
 
-	bam "github.com/cosmos/cosmos-sdk/baseapp" // Cosmos baseapp
+	bam "github.com/cosmos/cosmos-sdk/baseapp"     // Cosmos baseapp, implements ABCI
 	dbm "github.com/tendermint/tendermint/libs/db" // working with Tendermint db
 )
+
+const (
+	appName = "nameserviz"
+)
+
+type nameservizApp struct {
+	*bam.BaseApp
+}
+
+func NewnameservizApp(logger log.logger, db dbm.DB) *nameservizApp {
+
+	cdc := MakeCodec()
+
+	bApp := bam.NewBaseApp(appName, logger, db, auth.DefaultTxDecoder(cdc))
+
+	var app = &nameservizApp{
+		BaseApp: bApp,
+		cdc:     cdc,
+	}
+
+	return app
+}
