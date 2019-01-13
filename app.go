ae1e333d9f58cace5f8b34f818061abded43bebf
Clare Politano Hutchings <cpolitano8@gmail.com>
2019-01-13 15:15:13 -0600

    import cosmos sdk

---
+++ app.go
@@ -0,0 +1,9 @@
+package app
+
+import (
+  "github.com/tendermint/tendermint/libs/log" // Tendermint logger
+	"github.com/cosmos/cosmos-sdk/x/auth" // auth module
+
+	bam "github.com/cosmos/cosmos-sdk/baseapp" // Cosmos baseapp
+	dbm "github.com/tendermint/tendermint/libs/db" // working with Tendermint db
+)
