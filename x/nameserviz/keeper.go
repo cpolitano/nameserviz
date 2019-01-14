bc1f90c5f0c3e2a210c20f2dbd343a73710c6655
Clare Politano Hutchings <cpolitano8@gmail.com>
2019-01-14 14:29:44 -0600

    get/set names

--- x/nameserviz/keeper.go
+++ x/nameserviz/keeper.go
@@ -12,8 +12,21 @@ type Keeper struct {
   coinKeeper bank.coinKeeper
 
   namesStoreKey sdk.StoreKey // key to access namesStore
-  ownersStoreKey sdk.StoreKey // key to owners
-  pricesStoreKey sdk.StoreKey // key to prices
+  ownersStoreKey sdk.StoreKey // key to owner of a given name
+  pricesStoreKey sdk.StoreKey // key to price paid for name
 
   cdc *codec.Codec // wire codec for binary encoding/decoding
+}
+
+// sets the value string that a name resolves to
+func (k Keeper) SetName(ctx sdk.Context, name string, value string) {
+	store := ctx.KVStore(k.namesStoreKey)
+	store.Set([]byte(name), []byte(value)) // store only takes []byte
+}
+
+// returns the string that the name resolves to
+func (k Keeper) ResolveName(ctx sdk.Context, name string) string {
+	store := ctx.KVStore(k.namesStoreKey)
+	bz := store.Get([]byte(name)) // cast string to []byte
+	return string(bz) // cast []byte to string
 }
