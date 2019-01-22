1326d2b7f0a1b81dcf87863e80886e96df04f3dd
Clare Politano Hutchings <cpolitano8@gmail.com>
2019-01-21 19:30:01 -0600

    NewQuerier

---
+++ x/nameserviz/querier.go
@@ -0,0 +1,28 @@
+package nameserviz
+
+import (
+  "github.com/cosmos/cosmos-sdk/codec"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+// query endpoints supported by the governance Querier
+const (
+	QueryResolve = "resolve"
+	QueryWhois   = "whois"
+)
+
+// NewQuerier is the module level router for state queries
+func NewQuerier(keeper Keeper) sdk.Querier {
+	return func(ctx sdk.Context, path []string, req abci.RequestQuery) (res []byte, err sdk.Error) {
+		switch path[0] {
+		case QueryResolve:
+			return queryResolve(ctx, path[1:], req, keeper)
+		case QueryWhois:
+			return queryWhois(ctx, path[1:], req, keeper)
+		default:
+			return nil, sdk.ErrUnknownRequest("unknown nameservice query endpoint")
+		}
+	}
+}
