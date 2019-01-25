65171a1c408e9b5b25938f3f7132fd8e0c8421e5
Clare Politano Hutchings <cpolitano8@gmail.com>
2019-01-25 17:12:00 -0600

    nsd

---
+++ cmd/nsd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/tendermint/tendermint/libs/cli"
+	"github.com/tendermint/tendermint/libs/common"
+	"github.com/tendermint/tendermint/libs/log"
+
+	gaiaInit "github.com/cosmos/cosmos-sdk/cmd/gaia/init"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	app "github.com/cpolitano/nameserviz"
+	abci "github.com/tendermint/tendermint/abci/types"
+	cfg "github.com/tendermint/tendermint/config"
+	dbm "github.com/tendermint/tendermint/libs/db"
+	tmtypes "github.com/tendermint/tendermint/types"
+)
